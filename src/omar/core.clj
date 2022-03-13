d94241c2d9178aceedabae70f1ea7a565de5fef7
bigmuncha <mr.munchaev@mail.ru>
2022-03-13 13:31:23 +0300

    igor

--- src/omar/core.clj
+++ src/omar/core.clj
@@ -1,9 +1,11 @@
 (ns omar.core
   (:gen-class)
-  (:require
+  (:require [clojure.data.json ]
             [cheshire.core :refer :all]
             [omar.defines ]))
 
+(defn read-path [ ]
+  (str(read)))
 
 (defn get-file-list [directory]
   (-> directory clojure.java.io/file file-seq))
@@ -12,72 +14,54 @@
   (if (zero? (compare (-> substring reverse vec)
               (->> file-name reverse (take (count substring)) vec))) true false))
 
-(defn merge-includes [inc-list]
-  (str " -I" (clojure.string/join " -I" inc-list)))
-(defn merge-defines [def-list]
-  (str " -D" (clojure.string/join " -D" def-list)))
-(defn is-directory-have-include? [dir-name]
-  (not (empty? (filter #(file-match % ".h") (.list (clojure.java.io/file dir-name))))))
 
-(defn get-dir-list [project]
-  (filter #(.isDirectory %) (get-file-list project)))
-
-(defn get-include-list [dir-list]
-  (map str (filter #(is-directory-have-include? %) dir-list)))
-
-(defn final-get-include-list[project]
-  (concat
-   (-> project  get-dir-list get-include-list)
-   omar.defines/common-include))
-
-(defn create-command-str [global-struct filename]
-  (clojure.string/join " "
-                       (list
-                        (:compiler global-struct)
-                        (merge-includes (:includes global-struct))
-                        (merge-defines (:defines global-struct))
-                        (:flags global-struct)
-                        (:suffix global-struct )
-                        (str filename ".o" )
-                        filename
-                        )))
+(defn create-command [command filename]
+  (let [obj-file (str filename ".o")]
+    (-> (vector command obj-file filename)
+        flatten
+        vec)))
 
 (defn my-json [directory command file]
-     {:directory directory,
-      :command command
+     {:arguments command
+      :directory directory
       :file file})
 
+(defn helper [filename file-extension comp-list]
+  (if (file-match filename file-extension)
+    (my-json (:directory comp-list) (create-command (:arguments comp-list) filename)
+             filename)
+    nil))
 
-(defn create-global-struct [project]
-  {:compiler omar.defines/cppcompiler
-   :includes (final-get-include-list project)
-   :defines omar.defines/data-plane-defines
-   :flags omar.defines/cpp-flags
-   :suffix omar.defines/suffix
-   })
-
-
-(defn  json-file-logger [bigfilename comp-list]
-   (loop [json-list (my-main comp-list)]
-     (if (empty? json-list) nil
-         (do
-           (generate-stream
-            (first json-list)
-            (clojure.java.io/writer bigfilename :append true))
-             (spit bigfilename ",\n" :append true)
-             (recur (rest json-list))))))
-
-(defn my-main [project ]
-  (loop [
-         file-list (filter #(file-match % ".c" ) (get-file-list (second project)))
-         json-list '()
-         global-struct (create-global-struct project)
-         ]
+(defn my-main [comp-list]
+  (loop [ file-list (get-file-list (:directory comp-list))
+         json-list '() ]
     (if (empty? file-list) (filter #(not (nil? %)) json-list)
         (recur (rest file-list)
-               (cons
-                (my-json project
-                         (create-command-str global-struct (first file-list))
-                         (first file-list))
-                json-list)
-               global-struct))))
+               (cons (helper (str (first file-list)) ".cpp" comp-list)
+                     json-list)))))
+
+ (defn  json-file-logger [bigfilename comp-list]
+   (loop [json-list (my-main comp-list)]
+     (if (empty? json-list) nil
+         (do (generate-stream (first json-list)
+                          (clojure.java.io/writer bigfilename :append true)
+                          {:pretty true} )
+             (spit bigfilename ",\n" :append true)
+             (recur (rest json-list))
+             )
+         )))
+
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (json-file-logger "/home/omar/igor.json" omar.defines/data-plane))
+
+
+
+
+(defn merge-includes [inc-list]
+  (str " -I" (clojure.string/join " -I" inc-list)))
+
+(defn is-directory-have-include? [dir-name]
+  (not (empty? (filter #(file-match % ".h") (.list (clojure.java.io/file dir-name))))))
